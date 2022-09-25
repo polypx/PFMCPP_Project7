70dcaa47b7fc66fc24ea0c08e580bb4a9a843061
Dan Powell <dp@danielpowell.com>
2022-09-25 18:59:13 +0000

    baby steps with Dwarf

--- Dwarf.cpp
+++ Dwarf.cpp
@@ -1,7 +1,18 @@
 #include "Dwarf.h"
+#include "Utility.h"
 
-//Dwarf::Dwarf
+Dwarf::Dwarf(std::string name, int hp, int armour) : Character(hp, armour, 0), name(name)
+{
 
-//Dwarf::getName
+}
 
-//Dwarf::getStats
+const std::string& Dwarf::getName()
+{
+    return  name;
+}
+
+std::string Dwarf::getStats()         // using function from Utility.h
+{
+    return getCharacterStats(this);   // pass the Dwarf instance to getCharacterStats
+}
+
--- Dwarf.h
+++ Dwarf.h
@@ -1,7 +1,14 @@
 #pragma once
 
-struct Dwarf : Character
+#include "Character.h"
+
+struct Dwarf : public Character
 {
+    Dwarf(std::string n, int hp, int arm);
+
+    const std::string& getName() override;
+    std::string getStats() override;
+
 private:
     const std::string name;
 };
