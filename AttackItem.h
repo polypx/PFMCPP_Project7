380cb4fd86e66962a1046556aadc89f345b170a3
Dan Powell <dp@danielpowell.com>
2022-09-26 15:42:08 +0000

    Attack&Defensive items derived defines

--- AttackItem.h
+++ AttackItem.h
@@ -1,2 +1,9 @@
+#pragma once
 #include "Item.h"
+
+struct AttackItem : public Item
+{
+    AttackItem() : Item("attack item", 10) { }  // special attack item with 'factor' of 10
+    void use(Character* character) override;
+};
 
