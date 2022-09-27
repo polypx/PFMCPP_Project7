560cc1b63a92c8efb365a2fb13e3a30c3758f5f9
Dan Powell <dp@danielpowell.com>
2022-09-27 19:08:02 +0000

    revisions to Dragonslayer

--- DragonSlayer.h
+++ DragonSlayer.h
@@ -8,10 +8,11 @@ struct DragonSlayer : Character
     void attack(Character& other) override;
     const std::string& getName() override;  // rewrite the inherited getName
     std::string getStats() override;        // rewrite the inherited getStats
-    std::unique_ptr<AttackItem> attackItem = std::make_unique<AttackItem>();  // special Slayer attack item
 
 private:
     const std::string name;
+    std::unique_ptr<AttackItem> attackItem = std::make_unique<AttackItem>();  // special Slayer attack item
+
 };
 
 
